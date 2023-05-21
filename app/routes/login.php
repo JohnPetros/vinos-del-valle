48c034e826255c02653545f702b2f9054fc56c36
João Pedro <joaopedro.nc@outlook.com>
2023-05-21 11:28:05 -0300

    :pencil: Adding MIT license

--- app/routes/login.php
+++ app/routes/login.php
@@ -3,10 +3,6 @@
 use \App\controllers\LoginController;
 use \App\core\Response;
 
-$router->get('/page/{id}', function ($id) {
-  return new Response(200, 'Kaue' . $id);
-});
-
 $router->get('/', function () {
   return new Response(200, LoginController::getLoginPage());
 });
