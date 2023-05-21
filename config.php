3bb1aa3c4e5c98086f232bc57a273efc565b1ba0
João Pedro <joaopedro.nc@outlook.com>
2023-05-21 08:42:45 -0300

    :lock: Adding env vars

--- config.php
+++ config.php
@@ -2,6 +2,10 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use \App\controllers\LoginController;
+use App\controllers\LoginController;
+use App\core\Environment;
+
+// CARREGA VARIÁVEIS DE AMBIENTE
+Environment::load(__DIR__);
 
 echo LoginController::getLoginPage();
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'johnpetros/adega',
+        'name' => 'adega/vinos-del-valle',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '3acec7fb7a3f266167d5bbcd14cc9f288552d7a2',
+        'reference' => 'ac34ed1fc5d04804e7287223d1eaeddcfb835990',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'johnpetros/adega' => array(
+        'adega/vinos-del-valle' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '3acec7fb7a3f266167d5bbcd14cc9f288552d7a2',
+            'reference' => 'ac34ed1fc5d04804e7287223d1eaeddcfb835990',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
