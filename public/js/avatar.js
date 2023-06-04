5c8888d95d9f4e1064163681da01bf46ffb87233
João Pedro <joaopedro.nc@outlook.com>
2023-06-04 14:48:02 -0300

    :lipstick: style: rezise avatar img in user card

--- public/js/avatar.js
+++
@@ -1,7 +0,0 @@
-const avatars = document.querySelectorAll(".avatar");
-
-function setAvatar(avatar) {
-  avatar.src = `${PUBLIC_PATH}/uploads/avatars/${avatar.dataset.value}`;
-}
-
-avatars.forEach(setAvatar);
